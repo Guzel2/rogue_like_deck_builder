6aa6226577342f4be524d4e1f6be883441655a56
Guzel2 <pokelau2003@gmail.com>
2023-02-16 16:33:51 +0100

    working hand and dsicard pile

--- scenes_and_scripts/gameplay.gd
+++ scenes_and_scripts/gameplay.gd
@@ -1,4 +1,76 @@
 extends Node2D
 
-func play_card(card_name: String):
+onready var hand_cards = find_node('hand_cards')
+
+var deck = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var hand = []
+var discard = []
+
+func _ready():
+	draw_cards(1)
+
+func play_card(card_name: String, card):
 	print(card_name)
+	
+	var played = true
+	
+	match card_name: #check for conditions, for example if you need to discard cards
+		'0':
+			pass
+		'1':
+			played = false
+	
+	if played:
+		while true:
+			var hand_card = hand.pop_front()
+			
+			if hand_card == card_name:
+				break
+			
+			hand.append(hand_card)
+		
+		card.discard_this()
+		
+		match card_name:
+			'0':
+				print('test effect')
+				draw_cards(3)
+		
+	else:
+		card.return_to_hand()
+
+func start_turn():
+	pass
+
+func end_turn():
+	for card in hand_cards.get_children():
+		card.discard_this()
+	draw_cards(2)
+
+func draw_cards(num_of_cards):
+	var x = 0
+	
+	while x < num_of_cards:
+		if !deck.empty():
+			var new_card = deck.pop_front()
+			hand.append(new_card)
+			add_card_to_hand(new_card)
+			
+			x += 1
+		
+		else:
+			print(discard)
+			discard.shuffle()
+			deck = discard.duplicate()
+			discard.clear()
+			
+			x += 1
+
+func add_card_to_hand(card_name):
+	var card = load("res://scenes_and_scripts/gameplay/card.tscn").instance()
+	card.card_name = card_name
+	hand_cards.add_child(card)
+	hand_cards.organize_cards()
+
+func _on_end_turn_released():
+	end_turn()
--- scenes_and_scripts/gameplay/card.gd
+++ scenes_and_scripts/gameplay/card.gd
@@ -1,6 +1,6 @@
 extends TouchScreenButton
 
-onready var parent = get_parent()
+onready var parent = get_parent().get_parent()
 
 var card_name = 'test'
 
@@ -10,6 +10,7 @@ var just_pressed = false
 
 var start_pos
 var desired_pos
+var action_on_desired_pos = null
 
 var top_area_y = 150
 var play_area_y = 210
@@ -22,18 +23,20 @@ func _process(delta):
 	if is_pressed():
 		position = get_global_mouse_position() - size/2
 		just_pressed = true
+		print(card_name)
 	elif just_pressed:
 		just_pressed = false
 		
 		var y_pos = position.y + size.y/2
 		
 		if y_pos < top_area_y:
-			desired_pos = start_pos
+			return_to_hand()
 		elif y_pos < play_area_y:
+			action_on_desired_pos = null
 			desired_pos = position
-			parent.play_card(card_name)
+			parent.play_card(card_name, self)
 		else:
-			desired_pos = start_pos
+			return_to_hand()
 	
 	elif desired_pos != position:
 		var dir = (desired_pos - position)
@@ -41,3 +44,19 @@ func _process(delta):
 			position += dir/10 * (delta * 60)
 		else:
 			position = desired_pos
+			
+			match action_on_desired_pos:
+				null:
+					pass
+				'remove':
+					queue_free()
+
+func discard_this():
+	parent.discard.append(card_name)
+	desired_pos = Vector2(125, 230) - size/2
+	action_on_desired_pos = 'remove'
+
+func return_to_hand():
+	action_on_desired_pos = null
+	desired_pos = start_pos
+	
